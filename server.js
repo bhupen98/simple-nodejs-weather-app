21fd3576d707cd657ba34d1804763adb0aa7a552
bhupendra thapa <b707.bt@gmail.com>
2021-01-25 22:45:43 +1000

    added forecast features

--- server.js
+++ server.js
@@ -1,43 +1,47 @@
 const express = require('express');
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
-const request = require('request');
+const axios = require('axios');
 require('dotenv').config();
 const app = express()
 
 const apiKey = process.env.API_KEY;
-console.log(process.env)
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 //set template engine
 app.set('view engine', 'ejs')
 
-app.get('/', function (req, res) {
-  res.render('index', {weather: null, error: null});
-})
-
-app.post('/', function (req, res) {
-  let city = req.body.city;
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
-
-  request(url, function (err, response, body) {
-    if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
-    } else {
-      let weather = JSON.parse(body);
-      res.render('index',{weather: weather, error:null})
-      // if(weather.main == undefined){
-      //   res.render('index', {weather: null, error: 'Error, please try again'});
-      // } else {
-      //   let weatherText = `It's ${weather.main.temp} degrees in ${weather.name}!`;
-      //   res.render('index', {weather: weatherText, error: null});
-      // }
-    }
-  });
-})
 
 
-app.listen(PORT, function () {
-  console.log('Example app listening on port 3000!')
-})
+
+//get /
+  app.get('/', function (req, res) {
+    res.render('index', {weather: null, error: null});
+  })
+
+
+///post
+app.post('/',async (req, res)  => { 
+    let city = req.body.city;
+var currentUrl =`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+    var fiveDaysUrl =`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+
+try{
+var current = await axios.get(currentUrl);
+var fiveDays = await axios.get(fiveDaysUrl);
+  const date = new Date(fiveDays.data.list[0].dt)
+  
+  res.render('index', {weather:{current:current.data, fiveDays:fiveDays.data}, error:null})
+  
+}catch(err){
+  if(err){
+    res.render('index', {weather:null, error:`Error, ${err.message}`});
+    console.log(err.message)
+  }
+}    
+  })
+
+  app.listen(PORT, function () {
+    console.log('Example app listening on port 3000!')
+  })
